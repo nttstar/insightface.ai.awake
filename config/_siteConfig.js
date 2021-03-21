a6de266b380eafe5d0892dfeaa98e5d1a01dfdc1
Jia Guo <guojia@gmail.com>
2021-03-21 14:38:52 +0800

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -11,7 +11,7 @@ import theCMS from '../cms/netlify/cms'
 export default {
   siteName: 'Awake',
   tagline: 'A Nuxt.js Theme for Netlify CMS',
-  featureImage: '/uploads/home-hero.jpg',
+  featureImage: '/uploads/demo_image.jpg',
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
