3d7993e3a441247f6002e74d570a7b1b85084f24
Jia Guo <guojia@gmail.com>
2021-03-21 14:45:39 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,7 @@
 <template>
   <div id="home-page" class="page-wrapper home-page">
     <site-hero :title="title" :subtitle="subtitle" :image="featureImage">
-      <button
-        v-if="$siteConfig.newsletter.on"
-        class="button is-primary"
-        @click="$eventBus.$emit('modal-triggered', 'newsletter-modal')"
-      >
-        Subscribe To Newsletter
-      </button>
+
     </site-hero>
     <main-section theme="one-column">
       <template v-slot:default>
@@ -25,7 +19,7 @@
 <script>
 import { mapState } from 'vuex'
 import { setPageData } from '../helper'
-import NewsLetterFormModal from '~/components/NewsLetterFormModal'
+
 
 export default {
   name: 'HomePage',
@@ -34,9 +28,7 @@ export default {
       title: `Home | ${this.$siteConfig.siteName}`
     }
   },
-  components: {
-    NewsLetterFormModal
-  },
+
   computed: {
     ...mapState(['title', 'subtitle', 'featureImage'])
   },
